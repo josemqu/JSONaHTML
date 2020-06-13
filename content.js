f395559fd18912ae6149784ba8cb88e93faf5b1e
José <mailjmq@gmail.com>
2020-06-13 03:01:07 -0300

    se agregó la funcionalidad de descargar en formato CSV y Excel

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
-function GFG_FUN(obj) {
+function armarTabla(obj) {
   var cols = [];
   var table = document.createElement("table");
 
   if (obj instanceof Array) {
-
+    console.log(obj);
     var cols = [];
     // var table = document.createElement("table");
 
@@ -21,6 +21,7 @@ function GFG_FUN(obj) {
     // var table = document.createElement("table");
 
     // Create table row tr element of a table
+    table.id = "datos";
     var tr = table.insertRow(-1);
 
     for (var i = 0; i < cols.length; i++) {
@@ -47,14 +48,14 @@ function GFG_FUN(obj) {
     }
 
     // Add the newely created table containing json data
-    var el = document.getElementById("res");
+    // var el = document.getElementById("res");
 
 
     // el.innerHTML = "";
     // el.appendChild(table);
 
   } else if (obj instanceof Object) {
-    console.log(obj);
+    // console.log(obj);
     var prop = Object.keys(obj);
 
     for (var k = 0; k < prop.length; k++) {
@@ -86,7 +87,7 @@ function GFG_FUN(obj) {
 
       // Create the table header th element
       var td = document.createElement("td");
-      td.appendChild(GFG_FUN(obj[cols[i]]));
+      td.appendChild(armarTabla(obj[cols[i]]));
 
       // Append columnName to the table row
       tr.appendChild(td);
@@ -111,12 +112,92 @@ function load() {
     var child = document.body.children.length ? document.body.childNodes[0] : document.body;
     var data = child.innerText;
     var json = JSON.parse(data);
-    // console.log(json);
-    var table = GFG_FUN(json);
+    var table = armarTabla(json);
+
     content = '<link rel="stylesheet" type="text/css" href="' + chrome.runtime.getURL("style.css") + '">';
     document.body.innerHTML = content;
+
     document.body.appendChild(table);
+
+    var boton1 = document.createElement("a");
+    document.body.prepend(boton1);
+    boton1.innerText = "CSV";
+    boton1.id = "btnDownload";
+    boton1.href = "#";
+    boton1.onclick = descargarCSV();
+
+    var boton2 = document.createElement("a");
+    document.body.prepend(boton2);
+    boton2.innerText = "Excel";
+    boton2.id = "btnExcel";
+    boton2.href = "#";
+    boton2.onclick = fnExcelReport();
+
+    // var iframe = document.createElement("iframe");
+    // document.body.prepend(iframe);
+    // iframe.id = "txtArea1";
+    // iframe.style = "display:none";
+
   }
+}
+
+function descargarCSV() {
+  //-------EXPORTAR TABLA A CSV--------------------------------------------------------------------------
+  function exportTableToCSV() {
+    var csv = [];
+    var rows = document.querySelectorAll("#datos > tbody > tr");
+    for (var i = 0; i < rows.length; i++) {
+      var row = [], cols = rows[i].querySelectorAll("td, th");
+      for (var j = 0; j < cols.length; j++) {
+        row.push(cols[j].innerText);
+      };
+      row = row.join(";");
+      csv.push(row);
+    };
+    csv = csv.join('\n');
+    // console.log(csv);
+    return csv
+  }
+  //-------BAJAR TABLA A CSV--------------------------------------------------------------------------
+  var csvLink = document.querySelector('#btnDownload'),
+    csvBlob = new Blob([exportTableToCSV()], { type: "text/csv" }),
+    csvName = 'tabla.csv',
+    csvUrl = window.URL.createObjectURL(csvBlob);
+  csvLink.setAttribute('href', csvUrl);
+  csvLink.setAttribute('download', csvName);
+};
+
+//-------EXPORTAR TABLA A XLS--------------------------------------------------------------------------
+function fnExcelReport() {
+  var tab_text = "<table id='datos'><tbody><tr>";
+  var textRange; var j = 0;
+  tab = document.getElementById('datos'); // id of table
+
+  for (j = 0; j < tab.rows.length; j++) {
+    tab_text = tab_text + tab.rows[j].innerHTML + "</tr>";
+    //tab_text=tab_text+"</tr>";
+  }
+
+  tab_text = tab_text + "</table>";
+  tab_text = tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+  tab_text = tab_text.replace(/<img[^>]*>/gi, ""); // remove if u want images in your table
+  tab_text = tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
+
+  if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
+  {
+    txtArea1.document.open("txt/html", "replace");
+    txtArea1.document.write(tab_text);
+    txtArea1.document.close();
+    txtArea1.focus();
+    sa = txtArea1.document.execCommand("SaveAs", true, "Say Thanks to Sumit.xls");
+  }
+  else                 //other browser not tested on IE 11
+    sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));
+
+  return (sa);
 }
 
 load();
